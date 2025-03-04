889a9c80e968d72173f7b3c48d66626a9ed094e4
marcothedood <marco.medde@outlook.it>
2025-03-04 10:52:58 +0000

    alignment

--- report/timesheet_report.py
+++ report/timesheet_report.py
@@ -1,97 +1,80 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: Kavya Raveendran (odoo@cybrosys.com)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 from odoo import api, fields, models
+from collections import defaultdict
+from datetime import datetime
+
 
 class ReportTimesheet(models.AbstractModel):
     _name = 'report.timesheets_by_employee.report_timesheets'
     _description = 'Timesheet Report'
 
-    def get_timesheets(self, docs):
-        """input : name of employee, the starting date and ending date
-        output: timesheets by that particular employee within that period
-        """
-        if docs.from_date and docs.to_date:
-            record = self.env['account.analytic.line'].search(
-                [('user_id', '=', docs.user_id[0].id),
-                 ('date', '>=', docs.from_date), 
-                 ('date', '<=', docs.to_date)],
-                order='date')
-        elif docs.from_date:
-            record = self.env['account.analytic.line'].search(
-                [('user_id', '=', docs.user_id[0].id),
-                 ('date', '>=', docs.from_date)],
-                order='date')
-        elif docs.to_date:
-            record = self.env['account.analytic.line'].search(
-                [('user_id', '=', docs.user_id[0].id),
-                 ('date', '<=', docs.to_date)],
-                order='date')
-        else:
-            record = self.env['account.analytic.line'].search(
-                [('user_id', '=', docs.user_id[0].id)],
-                order='date')
+    def format_time_24h(self, hours):
+        """Convert float hours to 24h format string"""
+        total_minutes = int(hours * 60)
+        hours = total_minutes // 60
+        minutes = total_minutes % 60
+        return f"{hours:02d}:{minutes:02d}"
 
-        records = []
-        total = 0.0
+    def get_timesheets(self, docs):
+        domain = [('user_id', '=', docs.user_id[0].id)]
+        if docs.from_date:
+            domain.append(('date', '>=', docs.from_date))
+        if docs.to_date:
+            domain.append(('date', '<=', docs.to_date))
+
+        record = self.env['account.analytic.line'].search(domain, order='project_id, task_id, date')
+
+        timesheet_data = {
+            'projects': defaultdict(lambda: {'tasks': defaultdict(lambda: {'entries': [], 'subtotal': 0.0}), 'subtotal': 0.0}),
+            'total': 0.0,
+            'total_hours_display': '00:00'
+        }
 
         for rec in record:
-            # Converti la durata in formato HH:MM
-            hours = int(rec.unit_amount)
-            minutes = int((rec.unit_amount - hours) * 60)
-            duration_str = f"{hours:02d}:{minutes:02d}"
+            project_name = rec.project_id.name or 'No Project'
+            task_name = rec.task_id.name or 'No Task'
 
-            vals = {
-                'project': rec.project_id.name or '',
-                'user': rec.user_id.partner_id.name,
-                'duration': duration_str,
+            entry = {
                 'date': rec.date,
-                'description': rec.name or ''  # Aggiunta della descrizione
+                'description': rec.name or '',
+                'duration': self.format_time_24h(rec.unit_amount),
+                'hours': rec.unit_amount
             }
-            total += rec.unit_amount
-            records.append(vals)
 
-        # Converti il totale in formato HH:MM
-        total_hours = int(total)
-        total_minutes = int((total - total_hours) * 60)
-        total_str = f"{total_hours:02d}:{total_minutes:02d}"
+            timesheet_data['projects'][project_name]['tasks'][task_name]['entries'].append(entry)
+            timesheet_data['projects'][project_name]['tasks'][task_name]['subtotal'] += rec.unit_amount
+            timesheet_data['projects'][project_name]['subtotal'] += rec.unit_amount
+            timesheet_data['total'] += rec.unit_amount
 
-        return records, total_str
+        # Format subtotals
+        for project in timesheet_data['projects'].values():
+            project['subtotal_display'] = self.format_time_24h(project['subtotal'])
+            for task in project['tasks'].values():
+                task['subtotal_display'] = self.format_time_24h(task['subtotal'])
+
+        timesheet_data['total_hours_display'] = self.format_time_24h(timesheet_data['total'])
+
+        return timesheet_data
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        docs = self.env['timesheet.report'].browse(
-            self.env.context.get('active_id'))
-        
-        # Informazioni dipendente
-        identification = []
-        for rec in self.env['hr.employee'].search(
-                [('user_id', '=', docs.user_id[0].id)]):
-            if rec:
-                identification.append({
-                    'id': rec.id,
-                    'name': rec.name
-                })
+        docs = self.env['timesheet.report'].browse(self.env.context.get('active_id'))
+        company = self.env.company.sudo()
+        logo = False
+        if company.logo:
+            logo = company.logo
+        company_data = {
+            'name': company.name,
+            'email': company.email,
+            'city': company.city,
+            'street': company.street,
+            'zip': company.zip,
+            'state_id': company.state_id and company.state_id.name,
+            'phone': company.phone,
+            'website': company.website,
+        }
 
-        # Periodo
+        employee = self.env['hr.employee'].search([('user_id', '=', docs.user_id[0].id)], limit=1)
+
         period = None
         if docs.from_date and docs.to_date:
             period = f"From {docs.from_date} To {docs.to_date}"
@@ -100,13 +83,15 @@ class ReportTimesheet(models.AbstractModel):
         elif docs.to_date:
             period = f"To {docs.to_date}"
 
-        timesheets, total = self.get_timesheets(docs)
+        timesheet_data = self.get_timesheets(docs)
 
         return {
             'doc_ids': self.ids,
+            'doc_model': 'timesheet.report',
             'docs': docs,
-            'timesheets': timesheets,
-            'total': total,
-            'identification': identification,
+            'employee': employee,
             'period': period,
+            'timesheet_data': timesheet_data,
+            'res_company': company,
+            'company_data': company_data,
         }
